ccbf506f0917418a347c704e9975d27c289e5bee
Sachin <sgautam7507@gmail.com>
2026-01-30 11:24:55 +0530

    add book service dynamic route

---
+++ app/book-service/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>Book Service Page Working ✅</h1>
+      <p>Dynamic route is working</p>
+    </div>
+  );
+}
